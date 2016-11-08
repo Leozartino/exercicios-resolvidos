415159c7c9e34b0c68bdcbbb96e71cc8a397b8a1
Paulo Junior <pauloafpjunior@gmail.com>
2016-11-08 19:41:25 -0200

    simplificando o mesclar fila

--- mesclar-filas/Main.cpp
+++ mesclar-filas/Main.cpp
@@ -4,46 +4,39 @@
 
 using namespace std;
 
-void copiar(FilaVet* de, FilaVet* para) {
-	while(!estah_vazia(de)) {
-		inserir(para, remover(de));
-	}
-}
-
-FilaVet* mesclarFilas(FilaVet* f1, FilaVet* f2) {
-	FilaVet* fMesclada = criar_fila();
+FilaVet* mesclar_fila(FilaVet* f1, FilaVet* f2) {
+	FilaVet* fm = criar_fila();
+	
 	while(!estah_vazia(f1) && !estah_vazia(f2)) {
-		int iniF1 = obter_inicio(f1);
-		int iniF2 = obter_inicio(f2);
-		if (iniF1 < iniF2) {
-			inserir(fMesclada, remover(f1));
+		if (obter_inicio(f1) < obter_inicio(f2)) {
+			inserir(fm, remover(f1));
 		} else {
-			inserir(fMesclada, remover(f2));			
+			inserir(fm, remover(f2));
 		}
 	}
 	
-	copiar(f1, fMesclada);
-	copiar(f2, fMesclada);
+	while(!estah_vazia(f1)) { inserir(fm, remover(f1)); }
+	while(!estah_vazia(f2)) { inserir(fm, remover(f2)); }
 	
-	return fMesclada;
+	return fm;
+	
 }
 
 int main() {	
 	FilaVet* f1 = criar_fila();
 	FilaVet* f2 = criar_fila();
 	
-	inserir(f1, 1);
 	inserir(f1, 2);
-	inserir(f1, 3);
 	inserir(f1, 4);
-	inserir(f2, 0);
+	inserir(f1, 5);
+	inserir(f2, 1);
+	inserir(f2, 3);
 	
-	FilaVet* f3 = mesclarFilas(f1, f2);
+	FilaVet* fm = mesclar_fila(f1, f2);
 	
-	while(!estah_vazia(f3)) {
-		cout << remover(f3) << " ";
-	}
-	cout << endl;
+	while(!estah_vazia(fm)) {
+		cout << remover(fm) << endl;
+	}	
 	
 	return EXIT_SUCCESS;
 }
